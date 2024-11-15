6f565ff73e2ee2ffac9f96a061c3adc2052f8a82
sheenie <sheenie@student.chalmers.se>
2024-11-15 22:47:11 +0900

    #3 Set up App.vue to handle navigation between components

--- user-ui/src/App.vue
+++ user-ui/src/App.vue
@@ -1,85 +1,25 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <DentistList @select-dentist="dentistId = $event" />
+    <DentistTimetable v-if="dentistId" :dentistId="dentistId" @select-slot="slot = $event" />
+    <BookingForm v-if="slot" :dentistId="dentistId" :slot="slot" />
+    <BookingDetails />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import DentistList from './components/DentistList.vue';
+import DentistTimetable from './components/DentistTimetable.vue';
+import BookingForm from './components/BookingForm.vue';
+import BookingDetails from './components/BookingDetails.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default {
+  components: { DentistList, DentistTimetable, BookingForm, BookingDetails },
+  data() {
+    return {
+      dentistId: null,
+      slot: null
+    };
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+};
+</script>
