383324891def751cc2f9d9d124ddddc5476fccbc
sheenie <sheenie@student.chalmers.se>
2024-11-26 06:21:38 +0900

    #7 Added BookingDetailsView and separated business logic for BookingDetails component and view

--- user-ui/src/components/BookingDetails.vue
+++ user-ui/src/components/BookingDetails.vue
@@ -1,34 +1,27 @@
 <template>
-  <div v-if="booking">
-    <h2>Appointment Details</h2>
-    <p><strong>Time:</strong> {{ booking.time }}</p>
-    <p><strong>Date:</strong> {{ booking.date }}</p>
-    <p><strong>Clinic Name:</strong> {{ booking.clinicName }}</p>
-    <p><strong>Dentist:</strong> {{ booking.dentist }}</p>
-    <p><strong>Reference Code:</strong> {{ booking.referenceCode }}</p>
+  <div>
+    <div v-if="booking">
+      <h2>Appointment Details</h2>
+      <p><strong>Time:</strong> {{ booking.time }}</p>
+      <p><strong>Date:</strong> {{ booking.date }}</p>
+      <p><strong>Clinic Name:</strong> {{ booking.clinicName }}</p>
+      <p><strong>Dentist:</strong> {{ booking.dentist }}</p>
+      <p><strong>Reference Code:</strong> {{ booking.referenceCode }}</p>
+    </div>
+    <div v-else>
+      <p>No booking details available. Please enter a valid reference code.</p>
+    </div>
   </div>
 </template>
 
 <script>
-import api from '../patientApi.js';
-
 export default {
-  props: ['referenceCode'],
-  data() {
-    return {
-      booking: null
-    };
+  name: 'BookingDetails',
+  props: {
+    booking: {
+      type: Object,
+      required: false,
+    },
   },
-  async mounted() {
-    if (this.referenceCode) {
-      try {
-        const response = await api.getBooking(this.referenceCode);
-        this.booking = response.data;
-      } catch (err) {
-        console.error('Error fetching booking details:', err.message);
-        this.booking = null;
-      }
-    }
-  }
 };
 </script>
