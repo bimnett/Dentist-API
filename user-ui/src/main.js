97d64f57d7f43ef75bb48977e4901055bcf85c7d
sheenie <sheenie@student.chalmers.se>
2024-11-26 04:41:33 +0900

    #7 Fixed user-ui structure

---
+++ user-ui/src/main.js
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createBootstrap } from 'bootstrap-vue-next'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+import App from './patientApp.vue'
+import router from './router'
+
+const app = createApp(App)
+app.use(createBootstrap())
+app.use(router)
+
+app.mount('#app')
