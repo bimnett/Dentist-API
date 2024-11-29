d80a0f1604ee0745ba8497254cbfe9590e4a160f
sheenie <sheenie@student.chalmers.se>
2024-11-30 02:46:50 +0900

    #5 Refactored AvailableDates to separate view and component

---
+++ patient-ui/src/views/AvailableDatesView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1>Select a Date</h1>
+      <AvailableDates @date-selected="navigateToSlotSelection" />
+    </div>
+  </template>
+  
+  <script>
+  import AvailableDates from '../components/AvailableDates.vue';
+  
+  export default {
+    name: 'AvailableDatesView',
+    components: { AvailableDates },
+    methods: {
+      navigateToSlotSelection(selectedDate) {
+        this.$router.push({ name: 'SlotSelection', params: { selectedDate } });
+      },
+    },
+  };
+  </script>
