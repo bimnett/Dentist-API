f2ea15edb5cf2b31bbc0d54baf4422d3532e8c2d
Bimnet Tesfamariam <gustesbi@student.gu.se>
2024-11-29 12:16:58 +0100

    #6 Add map to homeview with markers for each dentist clinic

--- patient-ui/src/views/HomeView.vue
+++ patient-ui/src/views/HomeView.vue
@@ -3,10 +3,16 @@
       <h1>Welcome</h1>
       <p>Book an appointment with a dentist today</p>
     </div>
+    <DentistMap/>
   </template>
   
   <script>
+  import DentistMap from '../components/DentistMap.vue';
+
   export default {
     name: 'HomeView',
+    components: {
+      DentistMap
+    }
   };
   </script>
