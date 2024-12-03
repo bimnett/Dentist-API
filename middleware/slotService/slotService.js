c3f38cd1a89f6fa4343474042b7332390c55a39a
Ling Svahn <guslingsv@student.gu.se>
2024-12-03 12:31:10 +0100

    #9 Add initial logic for create, delete and update slots

--- middleware/slotService/slotService.js
+++ middleware/slotService/slotService.js
@@ -11,6 +11,112 @@ const options = {
 }
 
 const slotManagement = require('./src/slotManagement');
-
+/*
 console.log(slotManagement);  // Check what is being imported
-slotManagement.create_new_slot(options, mqtt, config); 
+var payload = slotManagement.create_new_slot(options, mqtt, config); 
+console.log(payload);*/
+
+options.clientId ='slotService_'+Math.random().toString(36).substring(2,10);
+// connect to broker 
+const client = mqtt.connect(config.BROKERURL, options);
+client.on('connect', () => {
+    console.log('Subscriber connected to broker');
+    // subscribe to recive data about new slot
+    const topic = '#';
+    client.subscribe(topic, { qos: 2 }, (err) => {
+        if (err) {
+            console.log('Subscription error:', err);
+        } else {
+            console.log(`Subscribed to topic: ${topic}`);
+        }
+    });
+});
+
+client.on('message', (topic, message) => {
+    switch(topic){
+        /*QUESTIONS 
+            - acces the db directly here or througth the db-handler?
+            - ok to update the db-hnadler? so it maches to what happens here?
+            - ok to update the topics?
+            - help the the ascyn programming
+            - referance code - shall I create it or shall we use mongoDb:s one
+        */
+
+        // create new avaliable time slot 
+        case config.topic_slot_management_create:
+    
+        
+        // LOOK UP ASYNCRONUS PROGRAMMING 
+            var time = slotManagement.validate_time(message);
+            var date = slotManagement.validate_date(message);
+            var clinic = slotManagement.validate_clinic(message);
+            var dentist = slotManagement.validate_dentist(message);
+        
+            var referenceCode = slotManagement.create_referance_code();
+
+            console.log(time,date,clinic,dentist);
+
+            // info given is ok --> cretae new slot 
+            // LOOK UP!! --> ASYNCRONUS PROGRAMMING
+            if(time && date && clinic && dentist){
+                slotManagement.create_new_slot(topic, message);
+            } else {
+                console.log(time);
+                console.log(date);
+                console.log(clinic);
+                console.log(dentist);
+            }
+
+            console.log("create new slot");
+           
+            break;
+        
+
+        //update info of an avaliable slot 
+        case config.topic_slot_management_update:
+
+            // validate info 
+            var time = slotManagement.validate_time(message);
+            var date = slotManagement.validate_date(message);
+            var clinic = slotManagement.validate_clinic(message);
+            var dentist = slotManagement.validate_dentist(message);
+            var referenaceCode = slotManagement.validate_referance_code(message);
+
+            // info ok --> update slot
+            if(time && date && clinic && dentist){
+                slotManagement.update_slot(topic, message);
+            } else {
+                console.log(time);
+                console.log(date);
+                console.log(clinic);
+                console.log(dentist);
+            }
+        
+            console.log("Update the slot");
+            break;
+        
+
+        //delte an avaliable slot 
+        case config.topic_slot_management_delete:
+
+            var referance_code = slotManagement.validate_referance_code(message);
+
+            console.log("delteSlot");
+            break;
+
+            
+        default:
+            console.log("topic: "+topic);
+            console.log("message:\n "+message);
+            break;
+    }
+     
+});
+
+client.on('error', (error) => {
+    console.log('Subscriber connection error:', error);
+});
+
+client.on('close', () => {
+    console.log('Subscriber connection closed');
+});
--- middleware/slotService/src/slotManagement.js
+++ middleware/slotService/src/slotManagement.js
@@ -1,40 +1,125 @@
 
 
 // dentist creats new avaliable time slot
-async function create_new_slot(options, mqtt, config){
-    options.clientId ='pub_slotService'+Math.random().toString(36).substring(2,10);
+async function create_new_slot(topic,message){
 
-    // connect to broker 
-    const client = mqtt.connect(config.brokerURL, options);
-
-    client.on('connect', () => {
-        console.log('Subscriber connected to broker');
-
-        const topic = config.topic_slot_management_create;
-        client.subscribe(topic, { qos: 2 }, (err) => {
-            if (err) {
-                console.log('Subscription error:', err);
-            } else {
-                console.log(`Subscribed to topic: ${topic}`);
-            }
-        });
-    });
-
-    client.on('message', (topic, message) => {
+    // when reciving info about the new slot
+    try{
+        var returnMessage= "The slot has been created (send confimation from db that the slot has been savde too)";
+        //convert message to json
+        const jsonMessage = JSON.parse(message);
+        /*
+        console.log("topic: "+topic);
+        console.log("message:\n" + jsonMessage);
         console.log(`Received message: + ${message} + on topic: + ${topic} :` + '\norigin : slotManagement.js');
+        */
+
+        const time = jsonMessage.time;
+        const date = jsonMessage.date;
+        const dentist = jsonMessage.dentist;
+        const clinic = jsonMessage.clinic;
+        console.log("create slot for this date: "+ date);
+        console.log("the slot has been created succesfully");
+
+        // only need to forward the info with pub to db with the rigth topic 
+
+
         
-    });
+        return returnMessage;
 
-    client.on('error', (error) => {
-        console.log('Subscriber connection error:', error);
-    });
-
-    client.on('close', () => {
-        console.log('Subscriber connection closed');
-    });
+    } catch (error){
+        console.log(error);
+    }
     
 };
 
+async function validate_time(message){
+    try {
+        var returnMessage = "";
+        const jsonMessage = JSON.parse(message);
+        const time = jsonMessage.time;
+        const date = jsonMessage.date;
+
+        const currentDate = new Date().toJSON().slice(0, 10);
+        const currentTime = new Date().toJSON().slice(15,20);
+
+        // valid time 00:00-23:59
+        if('00:00' < time || time > '23:59' ){
+            //send error message
+            returnMessage = "The time needs to be between 00:00-23:59";
+            return false;
+        }
+
+        // time have passed today, e.g today now 12:00 but insert today at 10:00
+        if(date == currentDate && time < currentTime){
+            returnMessage = "You have to choose a time later today";
+            return returnMessage;
+        }
+
+        // everything ok
+        return true;
+        
+    }catch(error){
+        console.log(error)
+    }
+};
+
+async function validate_date(message){
+    try {
+        var returnMessage = "";
+        const jsonMessage = JSON.parse(message);
+        const date = jsonMessage.date;
+        const currentDate = new Date().toJSON().slice(0, 10);
+
+        if(date<currentDate){
+            returnMessage = "This date has already passed";
+            return false;
+        }
+
+        //everything ok
+        return true;
+    }catch(error){
+        console.log(error);
+    }
+};
+
+async function validate_clinic(message){
+    // ask - shall we retriv the info from database handler or shall I acces the db directly here? 
+    return true;
+}
+
+async function validate_dentist(message){
+    // same qestion as above 
+    return true;
+}
+
+async function create_referance_code(){
+    return Math.random().toString(36).substring(2,10)
+}
+
+async function validate_referance_code(message){
+    // acces db here or via db-handler?
+}
+
+async function update_slot(message){
+    // question
+}
+
+async function delete_slot(message){
+    // same question as above 
+}
 
 
-module.exports = {create_new_slot}
+module.exports = {
+    create_new_slot, 
+    validate_time, 
+    validate_date,
+    validate_clinic, 
+    validate_dentist,
+
+    create_referance_code,
+    validate_referance_code,
+
+    update_slot,
+    delete_slot
+}
