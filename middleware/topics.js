3c94550daf009dd029ff6d6f813030473db7a178
Ling Svahn <guslingsv@student.gu.se>
2024-12-07 03:24:15 +0100

    #9 Remove un-used topics

--- middleware/topics.js
+++ middleware/topics.js
@@ -2,53 +2,6 @@ module.exports = {
         // test topic
         test:'topic/test',
         hello:'topic/hello',
-    
-        /*
-        // Database topics
-        database_insert: 'database/insert',
-        database_retrieve: 'database/retrieve',
-    
-        database_retrive_clinic: 'database/retrive/clinic',
-        database_retrive_dentist: 'database/retrive/clinic/dentists',
-        database_retrive_reference_code:'database/retrive/referance/code',
-
-        database_update: 'database/update',
-        database_insert_response: 'database/response/insert',
-        database_retrieve_response: 'database/response/retrieve',
-        database_update_response: 'database/response/update',
-    
-        // patient-related topics
-        notification_patient: 'notifications/patient',
-        notification_dentist: 'notifications/dentist',
-        slot_management_update: 'slot/update',
-        slot_management_delete: 'slot/delete',
-    
-        // booking and appointment related topics
-        booked_appointment_info: 'bookedappointment/appointment/info',
-        appointment_book: 'appointment/book',
-        appointment_cancel: 'appointment/cancel',
-    
-        // slot management related topics
-        slot_management_create: 'slots/createSlot',
-    
-        slot_management_created: 'slot/database/insert/new/created/slot',
-    
-        slot_dentist_avaliable: 'slots/dentist/avaliableSlots',
-        dentist_new_slots: 'slots/dentist/newslots',
-        clinic_dentist_slots: 'slots/clinic/dentist/avaliableSlots',
-    
-        slot_management_update: 'slot/update',
-        slot_managemnt_updated: 'slot/database/update/slot',
-    
-        slot_management_delete: 'slot/delete',
-        slot_management_deleted: 'slot/database/delte/slot',
-        
-        // clinics and dentists related topics
-        //all_clinics: 'clinics',
-        clinic_dentists: 'clinic/dentists',
-        appointments_dentist: 'appointments/dentist/bookedAppointments',
-        appointment_info: 'appointments/appointment/reference/info',
-        */
 
         // ---------------
 
