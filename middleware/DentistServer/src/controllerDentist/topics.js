ead55e7cbe3767aa662ca94f23f8a55a439622b3
Bimnet <bimnett@gmail.com>
2024-12-08 17:27:35 +0100

    #21 Add Dockerfile for dentistServer service

---
+++ middleware/DentistServer/src/controllerDentist/topics.js
@@ -0,0 +1,7 @@
+module.exports = {
+    appointments_dentist: 'slot/database/find/special/clinic', // Currently incorrect
+
+    create_new_slot: 'dentist/slot/create/new/slot',
+    update_slot: 'dentist/slot/update/slot',
+    delete_slot: 'dentist/slot/delete/slot',
+}
