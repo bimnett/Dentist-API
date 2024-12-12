820db4410862f7f0c9effa1aecf2dd363783f86a
Ling Svahn <guslingsv@student.gu.se>
2024-12-12 15:02:17 +0100

    #25 Restructure patientServer to enable tests

---
+++ middleware/patientServer/tests/dropdb.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+const credentials = require('../src/controllerPatient/credentials')
+
+// Variables
+var mongoURI = credentials.mongodb_url;
+
+// Drop database
+mongoose.connect(mongoURI).catch(function (err) {
+    if (err) {
+        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
+        console.error(err.stack);
+        process.exit(1);
+    }
+});
+mongoose.connection.dropDatabase().then(function () {
+    console.log(`Dropped database: ${mongoURI}`);
+    process.exit(0);
+});
