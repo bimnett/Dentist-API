dd1ee84c36170e7c1c318b8b04f39bc2d380eb68
Bimnet Tesfamariam <gustesbi@student.gu.se>
2024-12-17 21:51:31 +0100

    #23 Resolve MR comments for API URI's

--- patient-ui/src/api.js
+++ patient-ui/src/api.js
@@ -6,32 +6,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:80/api/patient',
+  baseURL: 'http://localhost:80/api/patients',
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
-// // get all clinics via MQTT
-// export const getClinics = async () => {
-//     try {
-//       const response = await api.get('/slots/bookAppointments/clinics');
-//       return response.data;
-//     } catch (error) {
-//       console.error('Error fetching clinics:', error);
-//       throw error;
-//     }
-//   };
-  
-//   // get dentists in a clinic via MQTT
-//   export const getDentistsByClinic = async (clinicId) => {
-//     try {
-//       const response = await api.get(`/slots/bookAppointments/clinics/${clinicId}/dentists`);
-//       return response.data;
-//     } catch (error) {
-//       console.error('Error fetching dentists:', error);
-//       throw error;
-//     }
-//   };
 
 export default api;
