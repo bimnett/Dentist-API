f5f9765b5cab037e15fe004f65d5c8db7ad19f69
Ling Svahn <guslingsv@student.gu.se>
2025-01-03 21:59:22 +0100

    #34 Restructure dentistApiServer

--- middleware/dentistApiServer/controllerDentist/scheduleApi.js
+++ middleware/dentistApiServer/controllerDentist/scheduleApi.js
@@ -8,7 +8,7 @@ const mqtt = require('mqtt');
 const express = require('express');
 const router = express.Router();
 const CREDENTIAL = require('../../credentials');
-const TOPIC = require('./topics');
+const TOPIC = require('../resources/topics');
 
 
 const options = {
