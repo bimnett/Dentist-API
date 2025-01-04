63b72e26314fd696146d10cd56637d0907771d51
Sheenie <sheenie@student.chalmers.se>
2025-01-05 04:39:01 +0900

    #34 Fix error messages in scheduleApi for dentistApiServer

--- middleware/patientServer/controllerPatient/availabilityApi.js
+++ middleware/patientServer/controllerPatient/availabilityApi.js
@@ -10,168 +10,72 @@ const options = {
     reconnectPeriod: 1000,  // Reconnect every 1 second if disconnected
 };
 
+// helper function to handle MQTT subscription
+function subscribeToTopic(clientIdPrefix, topic, res, successMessage) {
+    const clientId = clientIdPrefix + Math.random().toString(36).substring(2, 10);
+    const client = mqtt.connect(CREDENTIAL.brokerUrl, { ...options, clientId });
+
+    client.on('connect', () => {
+        console.log(`Subscriber connected to broker with clientId: ${clientId}`);
+        client.subscribe(topic, { qos: 2 }, (err) => {
+            if (err) {
+                console.error(`Subscription error for topic ${topic}:`, err);
+                res.status(500).json({ message: `Subscription error: ${err.message}` });
+                client.end(); // Close the connection on error
+            } else {
+                console.log(`Subscribed to topic: ${topic}`);
+            }
+        });
+    });
+
+    client.on('message', (subscribedTopic, message) => {
+        console.log(`Received message from topic ${subscribedTopic}: ${message}`);
+        res.status(200).json(JSON.parse(message.toString())); // Assuming JSON messages
+        client.end(); // Close the connection after processing the message
+    });
+
+    client.on('error', (error) => {
+        console.error(`MQTT client error for topic ${topic}:`, error);
+        res.status(500).json({ message: 'Could not connect to the broker' });
+    });
+
+    client.on('close', () => {
+        console.log(`Connection closed for clientId: ${clientId}`);
+    });
+}
 
 // get all clinics 
 router.get('/bookSlots/clinics', async function(req,res,next){
     try {
-       
-        options.clientId = "sub_patientServer"+Math.random().toString(36).substring(2,10);
-         // subscribe to the topic that will give us all the clinics 
-        const client = mqtt.connect(CREDENTIAL.brokerUrl, options);
-          
-        client.on('connect', () => {
-            console.log('Subscriber connected to broker');
-            const topic = TOPIC.all_clinics;
-            
-            client.subscribe(topic, { qos: 2 }, (err) => {
-                if (err) {
-                    console.log('Subscription error:', err);
-                } else {
-                    console.log(`Subscribed to topic: ${topic}`);
-                }
-            });
-        });
-          
-        client.on('message', (topic, message) => {
-            console.log(`Received message in json format: + ${message} + on topic: + ${topic}`);
-            // response to end user
-            return res.status(200).json(message);
-        });
-        
-        client.on('error', (error) => {
-            console.log('Subscriber connection error:', error);
-            return res.status(500).json({ message : "Unable to connect to the server"})
-        });
-        
-        client.on('close', () => {
-            console.log('Subscriber connection closed');
-            return res.status(200).json({message : "Closed connection"});
-        });
-
-    } catch(e){
-        return next(e);
+        subscribeToTopic('sub_patientServer', TOPIC.all_clinics, res, 'Fetched all clinics successfully');
+    } catch (err) {
+        next(err);
     }
 });
 
 // get all dentists in a specific clinic by clinicId
 router.get('/bookSlots/clinics/:clinicId/dentists', async function(req, res, next){
     try {
-        options.clientId = "sub_patienService"+Math.random().toString(36).substring(2,10); // random clientId
-        const client = mqtt.connect(CREDENTIAL.broker_url, options);
-
-        client.on('connect', () => {
-            console.log('Subscriber connected to broker');
-
-            const topic = TOPIC.clinic_dentists;
-            client.subscribe(topic, { qos: 2 }, (err) => {
-                if (err) {
-                    console.log('Subscription error:', err);
-                } else {
-                    console.log(`Subscribed to topic: ${topic}`);
-                }
-            });
-        });
-
-        client.on('message', (topic, message) => {
-            console.log(`Received message: + ${message} + on topic: + ${topic}`);
-            return res.status(200).json(message);
-        });
-
-        client.on('error', (error) => {
-            console.log('Subscriber connection error:', error);
-            return res.status(500).json({message: "Could not connect to server"});
-        });
-
-        client.on('close', () => {
-            console.log('Subscriber connection closed');
-            return res.status(200).json({message : "Closed connection"});
-        });
-
-    }catch(e){
-        return next(e);
+        subscribeToTopic('sub_patientService', TOPIC.clinic_dentists, res, 'Fetched all dentists for the clinic');
+    } catch (err) {
+        next(err);
     }
 });
 
 // get a dentist time-slots 
 router.get('/bookSlots/clinics/:clinicId/:dentistId/timeschedual', async function(req,res,next){
     try {
-        options.clientId = "sub_patientService"+Math.random().toString(36).substring(2,10); // random clientId
-        const client = mqtt.connect(CREDENTIAL.broker_url, options);
-
-        client.on('connect', () => {
-            console.log('Subscriber connected to broker');
-            const topic = TOPIC.clinic_dentist_slots;
-
-            client.subscribe(topic, { qos: 2 }, (err) => {
-                if (err) {
-                    console.log('Subscription error:', err);
-                } else {
-                    console.log(`Subscribed to topic: ${topic}`);
-                }
-            });
-        });
-
-        client.on('message', (topic, message) => {
-            console.log(`Received message: + ${message} + on topic: + ${topic}`);
-            return res.status(200).json(message);
-        });
-
-        client.on('error', (error) => {
-            console.log('Subscriber connection error:', error);
-            return res.status(500).json({message: "Could not connect to server"})
-        });
-
-        client.on('close', () => {
-            console.log('Subscriber connection closed');
-            return res.status(200).json({message : "Closed connection"});
-        });  
-
-    }catch(e){
-        return next(e);
+        subscribeToTopic('sub_patientService', TOPIC.clinic_dentist_slots, res, 'Fetched dentist time slots');
+    } catch (err) {
+        next(err);
     }
 });
 
-
-
-
 // get the info from a newly posted slot 
 router.get('/bookSlots', async function(req,res,next){
-
     try {
-
-        options.clientId =  'sub_patientService'+Math.random().toString(36).substring(2,10);
-        // connect to broker 
-        const client = mqtt.connect(CREDENTIAL.broker_url, options);
-    
-        client.on('connect', () => {
-            console.log('Subscriber connected to broker');
-            const topic = TOPIC.dentist_new_slots;
-
-            client.subscribe(topic, { qos: 2 }, (err) => {
-                if (err) {
-                    console.log('Subscription error:', err);
-                } else {
-                    console.log(`Subscribed to topic: ${topic}`);
-                }
-            });
-        });
-
-        client.on('message', (topic, message) => {
-            console.log(`Received message: + ${message} + on topic: + ${topic}`);
-            return res.status(200).json(message);
-        });
-
-        client.on('error', (error) => {
-            console.log('Subscriber connection error:', error);
-            return res.status(500).json({message: "Could not connect to server"})
-        });
-
-        client.on('close', () => {
-            console.log('Subscriber connection closed');
-            return res.status(200).json({message : "Closed connection"});
-        });
-
-    } catch(e) {
-        return next(e);
+        subscribeToTopic('sub_patientService', TOPIC.dentist_new_slots, res, 'Fetched new slot information');
+    } catch (err) {
+        next(err);
     }
 });
